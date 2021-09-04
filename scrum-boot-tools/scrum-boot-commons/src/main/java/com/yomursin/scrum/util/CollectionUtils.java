232b580e94eb8fc1a340a70b4858b97258713588
Bing D. Yee <bingdyee@gmail.com>
2021-09-04 22:49:14 +0800

    add commons module

---
+++ scrum-boot-tools/scrum-boot-commons/src/main/java/com/yomursin/scrum/util/CollectionUtils.java
@@ -0,0 +1,19 @@
+package com.yomursin.scrum.util;
+
+import java.util.Collection;
+
+/**
+ * @author yubinbin
+ * @since 2021/04/07
+ */
+public class CollectionUtils {
+
+    public static boolean isEmpty(Collection<?> coll) {
+        return (coll == null || coll.isEmpty());
+    }
+
+    public static boolean isNotEmpty(Collection<?> coll) {
+        return !CollectionUtils.isEmpty(coll);
+    }
+
+}
