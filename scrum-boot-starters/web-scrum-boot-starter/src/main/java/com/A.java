04e152605fe81b646dfa067856fdcdd0215fb226
Bing D. Yee <bingdyee@gmail.com>
2021-09-04 16:57:35 +0800

    init project

---
+++ scrum-boot-starters/web-scrum-boot-starter/src/main/java/com/A.java
@@ -0,0 +1,8 @@
+package com;
+
+/**
+ * @author Bing D. Yee
+ * @since 2021/09/04
+ */
+public class A {
+}
---
+++ scrum-boot-starters/web-scrum-boot-starter/src/test/java/com/yomursin/scrum/boot/web/MainTest.java
@@ -0,0 +1,13 @@
+package com.yomursin.scrum.boot.web;
+
+/**
+ * @author Bing D. Yee
+ * @since 2021/09/04
+ */
+public class MainTest {
+
+    public void one() {
+
+    }
+
+}
